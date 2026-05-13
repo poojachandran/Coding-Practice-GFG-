dec668d37a5fa57815560655075097f43e1f7f63
Pooja Chandran <pooja.poojachandran29@gmail.com>
2026-05-13 11:35:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print In Python/print-in-python.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+a = input()
+b = input()
+separator = input()[0]
+
+########### Write your code below ###############
+
+# Print with space
+print(f"{a} {b}")
+
+# Print without newline at the end
+print(f"{a} {b}",end ="")
+
+# Print with separator
+print(f"{a}{separator}{b}")
+
+# Print without space
+print(a,end="")
+print(b,end="")
+
+
+########### Write your code above ###############
