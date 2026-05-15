ee6c993437d7cf5d78d9b27af8d22e7fab0b6e74
Pooja Chandran <pooja.poojachandran29@gmail.com>
2026-05-15 10:51:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Day before N days/day-before-n-days.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findAnswer(self, d, n): 
+       #Code here
+       #if(d>n):
+         #  return (d-n)
+       #else:
+          # return ((n%7)//d)
+        return ((d-n)%7)
